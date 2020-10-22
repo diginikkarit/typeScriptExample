c52ddb52e62adafb3b9d3b8d1a660b2ab17e81ee
Heikki Mustonen <hmustone18@student.oulu.fi>
2020-10-22 15:56:21 +0300

    First real commit again

---
+++ Classes/Person.ts
@@ -0,0 +1,12 @@
+export default class Person{
+
+    firstName:string;
+    lastName:string;
+    salary:number
+
+    constructor(fName:any,lName:any,salary:any){
+        this.firstName = fName
+        this.lastName = lName
+        this.salary = salary
+    }
+}
